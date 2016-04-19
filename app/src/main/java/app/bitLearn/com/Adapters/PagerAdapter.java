01e2a2e7981d6a2f02ecd90c97bdee3f3d3668a3
akasiedu <akasiedu@gmail.com>
2016-04-19 15:50:36 +0000

    Add onboarding wizard to app

---
+++ app/src/main/java/app/bitLearn/com/Adapters/PagerAdapter.java
@@ -0,0 +1,27 @@
+package app.bitLearn.com.Adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by kwame on 4/19/16.
+ */
+public class PagerAdapter extends FragmentPagerAdapter {
+    int numOfTabs;
+
+    public PagerAdapter(FragmentManager fm, int numOfTabs) {
+        super(fm);
+        this.numOfTabs = numOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
